be112a014c153b7c3d444c99bff0b9828bc982bb
hurbeana <hurbeana@gmail.com>
2022-12-04 16:47:43 +0000

    added first version of packing script

---
+++ pack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tar -czf lab1_07.tgz \
+	exploit_heapcorruption-entry.py \
+	exploit_stackoverflow-medium.py \
+	vuln_heapcorruption-entry.c \
+	vuln_stackoverflow-medium.c \
+	Makefile
